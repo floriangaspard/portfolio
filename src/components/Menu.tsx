86218a51c9d59a4cfcf7996cad57b7c0522e7535
florian <floriangaspard.fg@gmail.com>
2023-11-19 12:47:01 +0100

    change menu behaviour

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,5 +1,5 @@
 import { faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faClose, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { MutableRefObject, useEffect, useRef, useState } from 'react';
 
@@ -9,12 +9,11 @@ type MenuProps = {
 
 export const Menu = ({ refs }: MenuProps) => {
   const [active, setActive] = useState(0);
-  const observers = useRef<IntersectionObserver[]>([]);
   const [toggled, setToggled] = useState(false);
 
-  const toggle = () => {
-    setToggled(!toggled);
-  };
+  const observers = useRef<IntersectionObserver[]>([]);
+  const menuRef = useRef<HTMLDivElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
     const obs: IntersectionObserver[] = [];
@@ -26,7 +25,7 @@ export const Menu = ({ refs }: MenuProps) => {
               setActive(i);
             }
           },
-          { threshold: 0.6 }
+          { threshold: 0.5 }
         );
         obs[i] = observers.current[i];
       }
@@ -41,60 +40,77 @@ export const Menu = ({ refs }: MenuProps) => {
     };
   }, [refs]);
 
+  useEffect(() => {
+    const handleClickOutside = ({ target }: MouseEvent) => {
+      if (
+        menuRef.current &&
+        !menuRef.current.contains(target as Node) &&
+        buttonRef.current &&
+        !buttonRef.current.contains(target as Node)
+      ) {
+        if (toggled) {
+          setToggled(false);
+        }
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [menuRef, toggled]);
+
   return (
-    <div className="sticky top-0 z-10 lg:w-1/4">
+    <>
       <button
+        ref={buttonRef}
         type="button"
         className="absolute top-0 items-center p-4 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden focus:outline-none z-20"
-        onClick={toggle}
+        onClick={() => setToggled(!toggled)}
       >
-        <svg
-          className="w-5 h-5"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 17 14"
-        >
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M1 1h15M1 7h15M1 13h15"
-          />
-        </svg>
+        <FontAwesomeIcon icon={toggled ? faClose : faBars} className="text-2xl" />
       </button>
-      <div
-        className={
-          (toggled ? 'flex ' : 'hidden lg:flex ') +
-          'absolute lg:sticky flex-col h-screen justify-center pl-7 top-0 w-80 md:w-1/2 lg:w-3/12 bg-white'
-        }
-      >
-        <ul className="text-3xl">
-          <li className={`p-3 ${active === 0 ? 'font-bold text-5xl' : ''}`}>
-            <a href="#about" onClick={() => setActive(0)}>
-              About
-            </a>
-          </li>
-          <li className={`p-3 ${active === 1 ? 'font-bold text-5xl' : ''}`}>
-            <a href="#experience" onClick={() => setActive(1)}>
-              Experience
-            </a>
-          </li>
-          <li className={`p-3 ${active === 2 ? 'font-bold text-5xl' : ''}`}>
-            <a href="#projects" onClick={() => setActive(2)}>
-              Projects
-            </a>
-          </li>
-        </ul>
-        <div className="flex gap-4 pl-7 mb-7 absolute bottom-0 left-0">
-          <a href="https://github.com/floriangaspard" target="_blank">
-            <FontAwesomeIcon icon={faGithub} className="text-3xl" />
-          </a>
-          <FontAwesomeIcon icon={faEnvelope} className="text-3xl" />
-          <FontAwesomeIcon icon={faInstagram} className="text-3xl" />
+      <div className="sticky top-0 z-10 lg:w-3/12 lg:h-screen">
+        <div
+          className={
+            'absolute lg:relative w-screen h-screen lg:w-3/12' +
+            (toggled ? ' bg-black bg-opacity-30' : '')
+          }
+        >
+          <div
+            ref={menuRef}
+            className={
+              (toggled ? 'opacity-100 ' : 'opacity-0 lg:opacity-100 ') +
+              'absolute flex flex-col h-screen justify-center pl-7 top-0 w-80 md:w-1/2 lg:w-3/12 bg-white transition-opacity ease-in-out duration-300'
+            }
+          >
+            <ul className="text-3xl">
+              <li className={`p-3 ${active === 0 ? 'font-bold text-5xl' : ''}`}>
+                <a href="#about" onClick={() => setActive(0)}>
+                  About
+                </a>
+              </li>
+              <li className={`p-3 ${active === 1 ? 'font-bold text-5xl' : ''}`}>
+                <a href="#experience" onClick={() => setActive(1)}>
+                  Experience
+                </a>
+              </li>
+              <li className={`p-3 ${active === 2 ? 'font-bold text-5xl' : ''}`}>
+                <a href="#projects" onClick={() => setActive(2)}>
+                  Projects
+                </a>
+              </li>
+            </ul>
+            <div className="flex gap-4 pl-7 mb-7 absolute bottom-0 left-0">
+              <a href="https://github.com/floriangaspard" target="_blank">
+                <FontAwesomeIcon icon={faGithub} className="text-3xl" />
+              </a>
+              <FontAwesomeIcon icon={faEnvelope} className="text-3xl" />
+              <FontAwesomeIcon icon={faInstagram} className="text-3xl" />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
