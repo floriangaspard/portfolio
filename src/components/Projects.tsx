7a68751e5f787e9460a3c1cbb67c604826c8e5ed
floarrd <floriangaspard.fg@gmail.com>
2025-03-24 16:36:30 +0100

    update projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -18,50 +18,27 @@ export const Projects = ({ refs }: ProjectsProps) => {
       <div className="">
         <div className="gap-10 grid grid-cols-1 md:grid-cols-2">
           <Project
-            title="DeFi Platform"
-            description="Development of a decentralized exchange platform on Ethereum enabling token swaps and liquidity pool management via smart contracts."
+            title="noo2.news"
+            description="A decentralized news platform built on the NOSTR protocol that analyzes trends with LLM models, featuring a modern, responsive interface and real-time updates."
             tags={[
               'React',
               'TypeScript',
+              'C#',
+              '.NET',
+              'MongoDB',
               'TailwindCSS',
-              'Solidity',
-              'Hardhat',
-              'Ethers',
-              'Ethereum',
               'Git',
               'Visual Studio Code',
             ]}
-            imgLink="/fswap.png"
-            link="https://fswap.floriangaspard.com"
-          />
-          <Project
-            title="NFT Marketplace"
-            description="Creation of an NFT marketplace on Stacks blockchain including management smart contract and sales user interface."
-            tags={[
-              'React',
-              'TypeScript',
-              'Shadcn-ui',
-              'Clarity',
-              'Stacks',
-              'Git',
-              'Visual Studio Code',
-            ]}
-            imgLink="/marxet.png"
-            link="https://marxet.floriangaspard.com"
+            imgLink="/noo2.png"
+            link="https://noo2.news"
           />
           <Project
             title="Portfolio"
-            description="Creation of a modern portfolio showcasing my projects and skills."
+            description="A responsive personal portfolio website with optimized performance and seamless navigation to showcase my professional experience and technical projects."
             tags={['React', 'Typescript', 'TailwindCSS', 'Shadcn-ui', 'Git', 'Visual Studio Code']}
             imgLink="/portfolio.png"
             link="https://floriangaspard.com"
-          />
-          <Project
-            title="Algorithmic trading platform"
-            description="Development of a complete algorithmic trading solution including backtesting, automated execution, and real-time performance monitoring."
-            tags={['Python', 'Flask', 'React', 'TypeScript', 'Redis', 'Git', 'Visual Studio Code']}
-            imgLink="crypto.jpeg"
-            link=""
           />
         </div>
       </div>
